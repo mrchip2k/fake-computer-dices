99217867886e1943aa6857c74bcd26e2d82231dd
mrchip2k <alemetzen3@gmail.com>
2023-06-18 21:51:35 +0200

    Fix whitespace not being printed

--- src/result_print/simple.rs
+++ src/result_print/simple.rs
@@ -2,6 +2,6 @@ use crate::multiroll::MultirollResult;
 
 pub fn print(data: &MultirollResult) {
     for dice in &data.rolls {
-        println!("{:8}{}", dice.0, dice.1)
+        println!("{:8}{}", dice.0.to_string(), dice.1)
     }
 }
